c984a1f5f0763d108d8e7b624c3068c171e4dab1
Aldo Brahmantyo <aldonugroho@gmail.com>
2022-06-15 22:53:14 +0700

    First push

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,18 @@
+<aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+        <li class="nav-item">
+            <a class="nav-link " href="index.html">
+                <i class="bi bi-grid"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="pages-faq.html">
+                <i class="bi bi-layout-text-window-reverse"></i>
+                <span>Menu</span>
+            </a>
+        </li>
+    </ul>
+</aside>
